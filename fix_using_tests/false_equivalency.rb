db2eeb2b0f7c5a7b775ea29be2ad6e052b0a078c
Mell Basham <mellbasham@gmail.com>
2020-12-01 06:27:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,4 @@
 # don't forget to add: require 'pry'
-
 def get_user_input
   gets.chomp
 end
@@ -11,14 +10,15 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
-  end
+  if num == 1
+    puts "YUM YUM MUNCH MUNCH MUNCH"
+  else if num == 2
+    puts "HAM HAM HAM IN MY TUMMY"
+    end
+  end 
 end
 
 def runner
   prompt_user
-  selection(get_user_input)
+  selection(get_user_input) 
 end
--- spec/false_equivalency_spec.rb
+++ spec/false_equivalency_spec.rb
@@ -3,6 +3,8 @@ require_relative '../fix_using_tests/false_equivalency'
 describe "false_equivalency" do
   it "`selection` returns the correct string based on user input" do
     expect(selection(1)).to eq("YUM YUM MUNCH MUNCH MUNCH")
+    print selection(1)
     expect(selection(2)).to eq("HAM HAM HAM IN MY TUMMY")
+    print selection(2)
   end
 end
